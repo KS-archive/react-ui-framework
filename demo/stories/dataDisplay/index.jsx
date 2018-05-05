ae4eae0e25be230a2ffad28035f60c8a0a1d409e
Konrad Szwarc <konrad.szwarc@outlook.com>
2018-05-05 17:10:12 +0200

    Create Spinner component

--- demo/stories/dataDisplay/index.jsx
+++ demo/stories/dataDisplay/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Index } from 'react-ui-framework';
 import Fab from './Fab';
+import Spinner from './Spinner';
 import Tooltip from './Tooltip';
 
 storiesOf('Data display', module)
   .addDecorator(story => <Index>{story()}</Index>)
   .add('Fab', () => <Fab />)
+  .add('Spinner', () => <Spinner />)
   .add('Tooltip', () => <Tooltip />);
